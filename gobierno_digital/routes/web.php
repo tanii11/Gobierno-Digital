b58747ce5bcd100198374ce9de190905ff7bbde7
Ana Tania Medina Espinoza <taniamedina@192.168.1.3>
2022-08-31 18:56:19 -0500

    crud funcional

--- gobierno_digital/routes/web.php
+++ gobierno_digital/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('users', UserController::class);
+
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
