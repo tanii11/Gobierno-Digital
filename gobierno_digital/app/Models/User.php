15470d6099891ff35812dab7edc5fafe9d728d29
Ana Tania Medina Espinoza <taniamedina@192.168.1.3>
2022-09-01 23:26:01 -0500

    correccion

--- gobierno_digital/app/Models/User.php
+++ gobierno_digital/app/Models/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable implements JWTSubject
     static $rules = [
 		'name' => 'required',
 		'email' => 'required',
-    'password' => 'required'
+        'password' => 'required'
     ];
 
     /**
